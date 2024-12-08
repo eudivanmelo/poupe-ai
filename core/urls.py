64a7e2590931a48e8fb30eae8ebe1f2bf104bcc0
Xande <alexandre12oliveira1@gmail.com>
2024-12-08 18:50:02 -0300

    home_page

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.home_page.urls')),
     path('auth/', include('apps.authentication.urls')),
     path('', include('apps.poupeai.urls')),
 ]
