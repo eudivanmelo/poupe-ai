cf703b84189489f91bc4c8abc8bcc60e2d00c7d2
eudivanmelo <eudivan44@gmail.com>
2025-01-10 08:24:34 -0300

    feat: added create end viewdetails modals for transactions page

--- static/js/transactions.js
+++ static/js/transactions.js
@@ -1,12 +1,28 @@
+const EditTransaction = (itemId) => {
+    // $('#recipient-name').val('Recipient ' + itemId);
+    // $('#message-text').val('Message content for transaction ' + itemId);
+
+    $('#editModal').modal('show');
+}
+
 var Alerts = (function () {
     var init_buttons = function () {
+        $('[id^="detail-transaction-"]').click(function (e) {
+            var itemId = $(this).data('item-id')
+            var modal = $('#detailModal')
+
+            modal.find('#modal-edit-button').click(() => {
+                modal.modal('hide')
+                EditTransaction(itemId)
+            })
+            
+            modal.modal('show')
+        })
+
         $('[id^="edit-transaction-"]').click(function (e) {
             var itemId = $(this).data('item-id')
 
-            $('#recipient-name').val('Recipient ' + itemId);
-            $('#message-text').val('Message content for transaction ' + itemId);
-
-            $('#editModal').modal('show');
+            EditTransaction(itemId)
         })
 
         $('[id^="delete-transaction-"]').click(function (e) {
@@ -29,8 +45,8 @@ var Alerts = (function () {
             }).then((Delete) => {
                 if (Delete) {
                     swal({
-                        title: 'Deleted!',
-                        text: 'Your file has been deleted.',
+                        title: 'Deletado!',
+                        text: 'A transação foi removida com sucesso.',
                         type: 'success',
                         buttons: {
                             confirm: {
