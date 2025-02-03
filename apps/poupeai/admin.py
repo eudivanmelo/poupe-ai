b3d625d2742e0bf63eb353662aa4a7d828afe902
eudivanmelo <eudivan44@gmail.com>
2025-02-03 08:36:13 -0300

    feat: created model to transaction

--- apps/poupeai/admin.py
+++ apps/poupeai/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.poupeai.models import Account, Goal, Category
+from apps.poupeai.models import Account, Goal, Category, Transaction
 
 class AccountAdmin(admin.ModelAdmin):
     model = Account
@@ -19,7 +19,13 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
     ordering = ['name', 'created_at']
     
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+    list_display = ['user', 'description', 'value', 'category', 'transaction_date', 'created_at']
+    search_fields = ['user', 'description']
+    ordering = ['user', 'description', 'created_at']
+    
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(Category, CategoryAdmin)
-
+admin.site.register(Transaction, TransactionAdmin)
---
+++ apps/poupeai/models/transaction.py
@@ -0,0 +1,18 @@
+from django.db import models
+from apps.authentication.models import CustomUser
+from apps.poupeai.models import Category
+
+class Transaction(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    description = models.CharField(max_length=255)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    fixed = models.BooleanField(default=False)
+    attachment = models.FileField(upload_to='attachments/', null=True, blank=True)
+    transaction_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.description
